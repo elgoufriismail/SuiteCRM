4340b0a20e848ca82412d08d163fb51d3b625eac
Jannat600 <81832036+Jannat600@users.noreply.github.com>
2022-07-27 16:52:08 +0200

    Board

--- include/language/fr_FR.lang.php
+++ include/language/fr_FR.lang.php
@@ -3523,7 +3523,7 @@ $app_strings['LBL_COLLECTION_TYPE'] = 'Type';
 
 $app_strings['LBL_ADD_TAB'] = 'Ajouter un onglet';
 $app_strings['LBL_EDIT_TAB'] = 'Modifier les onglets';
-$app_strings['LBL_SUITE_DASHBOARD'] = 'Tableau de bord CRM';
+$app_strings['LBL_SUITE_DASHBOARD'] = 'Tableau de bord';
 $app_strings['LBL_ENTER_DASHBOARD_NAME'] = 'Entrer le nom du tableau de bord :';
 $app_strings['LBL_NUMBER_OF_COLUMNS'] = 'Nombre de colonnes :';
 $app_strings['LBL_DELETE_DASHBOARD1'] = 'Etes-vous sûr de vouloir effacer ce';
